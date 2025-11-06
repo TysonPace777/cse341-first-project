6681796f114b21f0fcddf0bccacac6d2c5eb7275
tysonpace777 <32.tpace@gmail.com>
2025-11-06 12:31:52 -0700

    added routes for crud and swagger

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => (res.send("Hello World")));
 
+router.use('/', require('./swagger'));
 router.use('/contacts', require('./contacts'));
 
 
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const mongodb = require('./data/database');
+const bodyParser = require('body-parser');
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
 
 const app = express();
 
 const port = process.env.PORT || 8080;
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(bodyParser.json());
 app.use('/', require('./routes'));
-
 
 
 mongodb.initDb((err) => {
